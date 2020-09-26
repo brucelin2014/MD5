7eaf783fd081f27e3a7fb113a7fef1d4de0e5d11
brucelin2014 <linchunmeng511@163.com>
2020-09-26 16:50:32 +0800

    MD5算法练习

---
+++ MD5/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GEN_MD5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_md5 = new System.Windows.Forms.Button();
+            this.tb_src = new System.Windows.Forms.TextBox();
+            this.tb_des = new System.Windows.Forms.TextBox();
+            this.tb_des2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_md5
+            // 
+            this.btn_md5.Location = new System.Drawing.Point(13, 66);
+            this.btn_md5.Name = "btn_md5";
+            this.btn_md5.Size = new System.Drawing.Size(75, 23);
+            this.btn_md5.TabIndex = 0;
+            this.btn_md5.Text = "gen md5";
+            this.btn_md5.UseVisualStyleBackColor = true;
+            this.btn_md5.Click += new System.EventHandler(this.btn_md5_Click);
+            // 
+            // tb_src
+            // 
+            this.tb_src.Location = new System.Drawing.Point(13, 13);
+            this.tb_src.Multiline = true;
+            this.tb_src.Name = "tb_src";
+            this.tb_src.Size = new System.Drawing.Size(362, 47);
+            this.tb_src.TabIndex = 1;
+            // 
+            // tb_des
+            // 
+            this.tb_des.Location = new System.Drawing.Point(13, 95);
+            this.tb_des.Multiline = true;
+            this.tb_des.Name = "tb_des";
+            this.tb_des.Size = new System.Drawing.Size(362, 47);
+            this.tb_des.TabIndex = 2;
+            // 
+            // tb_des2
+            // 
+            this.tb_des2.Location = new System.Drawing.Point(12, 150);
+            this.tb_des2.Multiline = true;
+            this.tb_des2.Name = "tb_des2";
+            this.tb_des2.Size = new System.Drawing.Size(362, 47);
+            this.tb_des2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 209);
+            this.Controls.Add(this.tb_des2);
+            this.Controls.Add(this.tb_des);
+            this.Controls.Add(this.tb_src);
+            this.Controls.Add(this.btn_md5);
+            this.Name = "Form1";
+            this.Text = "MD5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_md5;
+        private System.Windows.Forms.TextBox tb_src;
+        private System.Windows.Forms.TextBox tb_des;
+        private System.Windows.Forms.TextBox tb_des2;
+    }
+}
+
---
+++ MD5/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GEN_MD5
+{
+    public partial class Form1 : Form
+    {
+        string src
+        {
+            get { return tb_src.Text; }
+            set { tb_src.Text = value; }
+        }
+        string des
+        {
+            get { return tb_des.Text; }
+            set { tb_des.Text = value; }
+        }
+        string des2
+        {
+            get { return tb_des2.Text; }
+            set { tb_des2.Text = value; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_md5_Click(object sender, EventArgs e)
+        {
+            des = GenerateMD5(src);
+        }
+
+        /// <summary>
+        /// MD5字符串加密
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns>加密后字符串</returns>
+        public static string GenerateMD5(string txt)
+        {
+            using (MD5 mi = MD5.Create())
+            {
+                byte[] buffer = Encoding.Default.GetBytes(txt);
+                //开始加密
+                byte[] newBuffer = mi.ComputeHash(buffer);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < newBuffer.Length; i++)
+                {
+                    sb.Append(newBuffer[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        /// <summary>
+        /// MD5流加密
+        /// </summary>
+        /// <param name="inputStream"></param>
+        /// <returns></returns>
+        public static string GenerateMD5(Stream inputStream)
+        {
+            using (MD5 mi = MD5.Create())
+            {
+                //开始加密
+                byte[] newBuffer = mi.ComputeHash(inputStream);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < newBuffer.Length; i++)
+                {
+                    sb.Append(newBuffer[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+    }
+
+}
